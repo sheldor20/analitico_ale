2ebae0598f1e86984d8c8653e147a36c686593f1
Kim Silvestre <eliakim.minichiello@gmail.com>
2026-09-10 08:53:00 -0300

    refactor: use standard authenticated entry point

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,4 @@
-import AdminAccessNotice from "@/components/admin-access-notice";
 import Dashboard from "@/components/dashboard";
-
 export default function Page() {
-  return (
-    <>
-      <AdminAccessNotice />
-      <Dashboard />
-    </>
-  );
+  return <Dashboard />;
 }
